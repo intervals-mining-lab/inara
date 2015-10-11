84ef202abc62d697eafb2b7a48ed26503041f3c9
Igor Rodionov <goruha@gmail.com>
2015-10-11 17:42:19 +0600

    Added support of vagrant

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-apt-get install texlive-latex-base texlive-lang-cyrillic texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended biblatex
-apt-get install chktex
-apt-get install zip
+apt-get install -y texlive-binaries texlive-latex-base texlive-lang-cyrillic texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended biblatex
+apt-get install -y chktex
+apt-get install -y zip unzip
 wget -P /tmp http://heanet.dl.sourceforge.net/project/disser/disser/1.3.2/disser-1.3.2.tds.zip
 unzip /tmp/disser-1.3.2.tds.zip -d /usr/share/texmf
 mktexlsr
